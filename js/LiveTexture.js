569426b3e50bf8354b0b1c5e4b050c53635365bc
Bryce Evans <bae43@cornell.edu>
2020-10-27 02:33:24 -0400

    cleanup

--- js/LiveTexture.js
+++ js/LiveTexture.js
@@ -1,51 +1,41 @@
 import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 
-function init()
-{
-    renderer = new THREE.WebGLRenderer( {antialias:true} );
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    renderer.setSize (width, height);
-    document.body.appendChild (renderer.domElement);
-
-    scene = new THREE.Scene();
-    
-    var cubeGeometry = new THREE.BoxGeometry (10,10,10);
-    var cubeMaterial = new THREE.MeshBasicMaterial ({color: 0x1ec876});
-    cube = new THREE.Mesh (cubeGeometry, cubeMaterial);
-
-    cube.position.set (0, 0, 0);
-    scene.add (cube);
-
-    camera = new THREE.PerspectiveCamera (45, width/height, 1, 10000);
-    camera.position.y = 160;
-    camera.position.z = 400;
-    camera.lookAt (new THREE.Vector3(0,0,0));
-
-    controls = new OrbitControls (camera, renderer.domElement);
-    
-    var gridXZ = new THREE.GridHelper(100, 10, new THREE.Color(0xff0000), new THREE.Color(0xffffff));
-    scene.add(gridXZ);
-
-}
 
 function main() {
 
+  var renderer = new THREE.WebGLRenderer( {antialias:true} );
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  renderer.setSize (width, height);
+  document.body.appendChild (renderer.domElement);
+
+  var scene = new THREE.Scene();
+  
+  var cubeGeometry = new THREE.BoxGeometry (10,10,10);
+  var cubeMaterial = new THREE.MeshBasicMaterial ({color: 0x1ec876});
+  var cube = new THREE.Mesh (cubeGeometry, cubeMaterial);
+
+  cube.position.set (0, 0, 0);
+  scene.add (cube);
+
+  var camera = new THREE.PerspectiveCamera (45, width/height, 1, 10000);
+  camera.position.y = 160;
+  camera.position.z = 400;
+  camera.lookAt (new THREE.Vector3(0,0,0));
+
+  var controls = new OrbitControls (camera, renderer.domElement);
+  
+  var gridXZ = new THREE.GridHelper(100, 10, new THREE.Color(0xff0000), new THREE.Color(0xffffff));
+  scene.add(gridXZ);
+
   var bufferScene = new THREE.Scene();
-  // var scene = new THREE.Scene();
 
   var width = 512;
   var height = 512;
   var buffer_camera = new THREE.PerspectiveCamera(70, width / height, 1, 1000);
   var main_camera = new THREE.PerspectiveCamera(70, width / height, 1, 1000);
 
-  // var renderer = new THREE.WebGLRenderer();
-  // renderer.setSize(width, height);
-  // document.body.appendChild(renderer.domElement);
 
-
-  // Scene to render with 
-  
   var bufferTexture = new THREE.WebGLRenderTarget(width, height, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter });
 
   var redMaterial = new THREE.MeshBasicMaterial({ color: 0xF06565 });
@@ -74,35 +64,24 @@ function main() {
 
   function render() {
 
-    // controls.update();
-    // requestAnimationFrame(render);
-
-    
+    controls.update();
+    requestAnimationFrame(render);    
 
     //Make the box rotate on box axises
     boxObject.rotation.y += 0.01;
     boxObject.rotation.x += 0.01;
     //Rotate the main box too
-    mainBoxObject.rotation.y += 0.01;
-    mainBoxObject.rotation.x += 0.01;
+    mainBoxObject.rotation.y -= 0.01;
+    mainBoxObject.rotation.x -= 0.01;
 
     renderer.setRenderTarget(bufferTexture)
     renderer.render(bufferScene, buffer_camera);
 
     renderer.setRenderTarget(null)
-    // renderer.render(scene, main_camera);
-
-
-    controls.update();
-    requestAnimationFrame ( render );  
     renderer.render (scene, camera);
 
   }
   render();
 }
 
-var scene, renderer, camera;
-var cube;
-var controls;
-init();
-main()
+main();
